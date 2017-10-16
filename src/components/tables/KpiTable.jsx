8235e1171b15e02cf87570f7f98c38ae7856d7bd
liubenjun <EX-LIUBENJUN002@pingan.com.cn>
2017-10-16 20:31:15 +0800

    no message

--- src/components/tables/KpiTable.jsx
+++ src/components/tables/KpiTable.jsx
@@ -9,7 +9,7 @@ class KpiTable extends React.Component {
 
     componentDidMount() {
         this.timerID = setInterval(
-            () =>this.tick(),
+            () => this.tick(),
             1000
         );
     }
@@ -17,18 +17,20 @@ class KpiTable extends React.Component {
     componentWillUnmount() {
         clearInterval(this.timerID);
     }
+
     tick() {
         this.setState({
             date: new Date()
         });
     }
+
     render() {
         return (
             <div>
-                <h1>Hello, word</h1>
-                <h2>it is {this.state.date.toLocaleDateString()}.</h2>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
-        )
+        );
     }
 }
 
